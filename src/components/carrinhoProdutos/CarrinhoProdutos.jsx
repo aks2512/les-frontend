04e748c9950b9cacc1189ed764b60824efe096e6
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-02 15:59:41 -0300

    partial: cart

--- src/components/carrinhoProdutos/CarrinhoProdutos.jsx
+++ src/components/carrinhoProdutos/CarrinhoProdutos.jsx
@@ -1,19 +1,42 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import api from "../../api";
 import { CarrinhoProduto } from "./partials/CarrinhoProduto";
 
 import './style.scss';
 
 export function CarrinhoProdutos() {
+    const [cart, setCart] = useState([]);
+
+    async function getCart() {
+        try {
+            const response = await api.get('/carts', {
+                isOpen: true
+            });
+            setCart(response.data[0]);
+        } catch (error) {
+            toast.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getCart()
+    }, [cart]);
+
     return (
         <div className="carrinho-produtos">
             <div className="produtos">
-                <CarrinhoProduto/>
-                <CarrinhoProduto/>
-                <CarrinhoProduto/>
-                <CarrinhoProduto/>
+                {
+                    cart?.items?.length && cart.items.length > 0 ? cart.items.map(item => {
+                        return (<CarrinhoProduto key={item.id} item={item}/>)
+                    }) : (
+                        <div className="sem-produtos">Sem Produtos</div>
+                    )
+                }
             </div>
             <div className="total">
                 <p>Total</p>
-                <p><strong>R$ 99,00</strong></p>
+                <p><strong>R$ {cart.total_price}</strong></p>
             </div>
         </div>
     );
--- src/components/carrinhoProdutos/partials/CarrinhoProduto.jsx
+++ src/components/carrinhoProdutos/partials/CarrinhoProduto.jsx
@@ -1,38 +1,59 @@
-import { useState } from 'react';
-import produto from '../../../assets/imgs/lego.svg';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import api from '../../../api';;
 
-export function CarrinhoProduto() {
-    const [amount, setAmount] = useState('1');
+export function CarrinhoProduto(itemPass) {
+    const [item, setItem] = useState({});
 
     function addAmount() {
-        if (amount !== '99') setAmount((parseInt(amount) + 1).toString());
+        if (item.quantity < 100) setItem({
+            ...item, 
+            quantity: item.quantity + 1
+        }, updateItem());
     }
 
     function rmvAmount() {
-        if (amount !== '1') setAmount((parseInt(amount) - 1).toString());
+        if (item.quantity > 1) setItem({
+            ...item, 
+            quantity: item.quantity - 1
+        }, updateItem());
     }
+    
+    async function updateItem() {
+        try {
+            console.log(item);
+            const response = await api.put(`/cart-items/${item.id}`, item);
+            toast.success(response.data.message);
+        } catch (e) {
+            toast.error(e.response.data.message);
+        }
+    }
+
+    useEffect(() => {
+        setItem(itemPass.item);
+    }, []);
 
     return (
         <div className="carrinho-produto">
             <button className="remove-produto">x</button>
             <div className="body">
                 <div className="image">
-                    <img src={produto} alt="" />
+                    <img src={'http://localhost:3333/files/' + (item?.product?.image || 'default.png')} alt="" />
                 </div>
                 <div className="content">
-                    <div className="title">Jogo Uma  Aventura Movie  Lego 2 </div>
+                    <div className="title">{item?.product?.name}</div>
                     <fieldset className="amount">
                         <button onClick={rmvAmount} className="remove">-</button>
                         <input 
                             type="text"
-                            value={amount}
+                            value={item?.quantity}
                             maxLength="2"
                             disabled={true}
                         />
                         <button onClick={addAmount} className="add">+</button>
                     </fieldset>
                 </div>
-                <div className="price"><p>R$ 49,90</p></div>
+                <div className="price"><p>R$ {item?.price}</p></div>
             </div>
         </div>
     );
