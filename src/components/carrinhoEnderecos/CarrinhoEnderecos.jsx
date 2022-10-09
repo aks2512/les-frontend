890231f9a13c95148954da6e28de0484a7103a71
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-09 12:37:37 -0300

    partial: cart addresses

--- src/components/carrinhoEnderecos/CarrinhoEnderecos.jsx
+++ src/components/carrinhoEnderecos/CarrinhoEnderecos.jsx
@@ -1,4 +1,5 @@
-import { useContext } from "react";
+import { useEffect } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../../contexts/AuthContext";
 
@@ -8,25 +9,44 @@ import './style.scss';
 
 export function CarrinhoEnderecos() {
     const { user } = useContext(Context);
+    const [paymentAddress, setPaymentAddress] = useState();
+    const [deliveryAddress, setDeliveryAddress] = useState();
+
+    useEffect(() => {
+        if (user) {
+            console.log(user);
+            setPaymentAddress(user?.person?.addresses?.find(address => address.address_type.id === 1));
+            setDeliveryAddress(user?.person?.addresses?.find(address => address.address_type.id === 2));
+        }
+    }, [user]);
+
     return (
         <div className="carrinho-enderecos">
             <div className="d-block d-md-flex">
                 <div className="enderecos col-12 col-md-6">
                     <h4>Endereços</h4>
-                    {
-                        user?.person?.addresses?.map(address => (
-                            <CarrinhoEndereco
-                                name={address.name}
-                                place={address.place}
-                                city={address.city}
-                                state={address.state}
-                                cep={address.cep}
-                            />
-                        ))
-                    }
+                    <h5>Cobrança</h5>
+                    { paymentAddress ? <CarrinhoEndereco
+                        type={paymentAddress?.address_type}
+                        name={paymentAddress?.name}
+                        place={paymentAddress?.place}
+                        city={paymentAddress?.city}
+                        state={paymentAddress?.state}
+                        cep={paymentAddress?.cep}
+                    /> : <p className="text-center">Nenhum endereço cadastrado</p> }
+                    <h5>Entrega</h5>
+                    { deliveryAddress ? <CarrinhoEndereco
+                        type={deliveryAddress?.address_type}
+                        name={deliveryAddress?.name}
+                        place={deliveryAddress?.place}
+                        city={deliveryAddress?.city}
+                        state={deliveryAddress?.state}
+                        cep={deliveryAddress?.cep}
+                    /> : <p className="text-center">Nenhum endereço cadastrado</p> }
                 </div>
                 <div className="novo-endereco col-12 col-md-6">
                     <Link to="/register-address">Cadastrar novo endereco</Link>
+                    <button className="select-address">Selecionar Endereço</button>
                 </div>
             </div>
         </div>
--- src/components/carrinhoEnderecos/partials/CarrinhoEndereco.jsx
+++ src/components/carrinhoEnderecos/partials/CarrinhoEndereco.jsx
@@ -1,12 +1,13 @@
 import localizacao from '../../../assets/imgs/localizacao.svg';
 
-export function CarrinhoEndereco({name, place, city, state, cep }) {
+export function CarrinhoEndereco({ name, place, city, state, cep, type }) {
     return (
         <div className="carrinho-endereco">
             <div className="image">
                 <img src={localizacao} alt=""/>
-            </div>
+            </div> 
             <div className="content">
+                <p className="type">{type.name}</p>
                 <p className="name">{name}</p>
                 <p className="information1">{place}</p>
                 <p className="information2">{city}, {state} - CEP {cep}</p>
