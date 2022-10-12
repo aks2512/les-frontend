0ac5d6c19cae29abc6e103acf1193d3bc7c83d04
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-12 14:49:32 -0300

    fix: modularize Card Register

---
+++ src/components/cartBody/CartBody.jsx
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import api from "../../api";
+import { Context } from "../../contexts/AuthContext";
+import { CarrinhoEnderecos } from "../carrinhoEnderecos/CarrinhoEnderecos";
+import { CarrinhoProdutos } from "../carrinhoProdutos/CarrinhoProdutos";
+import { Pagamento } from "../pagamento/Pagamento";
+import { WhiteBox } from "../whiteBox/WhiteBox";
+
+import './style.scss'
+
+export function CartBody() {
+    const { cart } = useContext(Context);
+    const [cards, setCards] = useState([]);
+    const [paymentAddress, setPaymentAddress] = useState();
+    const [deliveryAddress, setDeliveryAddress] = useState();
+
+    async function onSubmit(){
+        try{
+            const activeCards = cards.filter(c => c.active)
+            const response = await api.post('/purchases', {
+                cart_id: cart.id,
+                payment_address: paymentAddress,
+                deliveryAddress: deliveryAddress,
+                cards: activeCards,
+            })
+
+            toast(response.data.message)
+        } catch(err){
+            toast.error(err.response.data.message || 'Falha ao finalizar compra');
+        }
+    }
+
+    return (
+        <>
+            <WhiteBox>
+                <h3>Carrinho</h3>
+                <div className="d-flex flex-wrap">
+                    <div className="col-12 col-xl-8 mb-2">
+                        <CarrinhoProdutos/>
+                        <CarrinhoEnderecos
+                            deliveryAddress={deliveryAddress}
+                            paymentAddress={paymentAddress}
+                            setDeliveryAddress={setDeliveryAddress}
+                            setPaymentAddress={setPaymentAddress}
+                        />
+                    </div>
+                    <div className="col-12 col-xl-4 mb-2">
+                        <Pagamento cards={cards} setCards={setCards} onSubmit={onSubmit}/>
+                        <div className="d-flex justify-content-center create-purchase">
+                            <button className="btn-finalizar" onClick={(e) => {onSubmit(e)}}>Finalizar compra</button>
+                        </div>
+                    </div>
+                </div>
+            </WhiteBox>
+        </>
+    );
+}
--- src/components/pagamento/partials/Cartao.jsx
+++ src/components/pagamento/partials/Cartao.jsx
@@ -1,9 +1,11 @@
+import { Children } from 'react';
 import mastercard from '../../../assets/imgs/mastercard.svg';
 
-export function Cartao({name, image}) {
+export function Cartao({name, image, children}) {
 
     return (
         <div className="cartao" >
+            {children}
             <div className="image">
                 <img src={mastercard} alt="" />
             </div>
