5a54d51060074a46e6b48dbf2a8be5024907fe88
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-12 16:48:22 -0300

    partial: meus pedidos

--- src/pages/MeusPedidos.jsx
+++ src/pages/MeusPedidos.jsx
@@ -6,9 +6,27 @@ import { SideMenu } from "../components/sideMenu/SideMenu";
 import { WhiteBox } from "../components/whiteBox/WhiteBox";
 
 import pedido from '../assets/imgs/pedido.svg';
+import { toast } from "react-toastify";
+import api from "../api";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export function MeusPedidos() {
+    const [purchases, setPurchases] = useState([])
 
+    async function loadPurchases() {
+        try {
+            const response = await api.get(`purchases`);
+            console.log(response.data)
+            setPurchases(response.data);
+        } catch(err) {
+            toast.error(err.response.data.message || 'Falha ao carregar pedidos');
+        }
+    }
+
+    useEffect(() => {
+        loadPurchases()
+    }, [])
     return (
         <>
             <Header/>
@@ -24,103 +42,51 @@ export function MeusPedidos() {
                                     <img src={pedido} alt="" />
                                     <h4>Pedidos</h4>
                                 </div>
-
-                                <div className="accordion">
-                                    <input className="accordion-radio" id="accordion-radio" type="checkbox" />
-                                    <label htmlFor="accordion-radio" className="accordion-header">
-                                        <div>Pedido: xxxxxxxxxxx</div>
-                                        <div>Status: entregue</div>
-                                        <div>R$ 49,90</div>
-                                    </label>
-                                    <div className="accordion-body">
-
-                                        <div className="table-container">
-                                            <table>
-                                                <thead>
-                                                    <tr>
-                                                        <th>Produto</th>
-                                                        <th>Valor</th>
-                                                        <th>Quantidade</th>
-                                                        <th></th>
-                                                        <th></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>xxxxxxx</td>
-                                                        <td>R$ 49,90</td>
-                                                        <td className="amount">5</td>
-                                                        <td>trocar</td>
-                                                        <td>cancelar</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>xxxxxxx</td>
-                                                        <td>R$ 49,90</td>
-                                                        <td className="amount">5</td>
-                                                        <td>trocar</td>
-                                                        <td>cancelar</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>xxxxxxx</td>
-                                                        <td>R$ 49,90</td>
-                                                        <td className="amount">5</td>
-                                                        <td>trocar</td>
-                                                        <td>cancelar</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="accordion">
-                                    <input className="accordion-radio" id="accordion-radio2" type="checkbox" />
-                                    <label htmlFor="accordion-radio2" className="accordion-header">
-                                        <div>Pedido: xxxxxxxxxxx</div>
-                                        <div>Status: entregue</div>
-                                        <div>R$ 49,90</div>
-                                    </label>
-                                    <div className="accordion-body">
-                                        
-                                        <div className="table-container">
-                                            <table>
-                                                <thead>
-                                                    <tr>
-                                                        <th>Produto</th>
-                                                        <th>Valor</th>
-                                                        <th>Quantidade</th>
-                                                        <th></th>
-                                                        <th></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>xxxxxxx</td>
-                                                        <td>R$ 49,90</td>
-                                                        <td>5</td>
-                                                        <td>trocar</td>
-                                                        <td>cancelar</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>xxxxxxx</td>
-                                                        <td>R$ 49,90</td>
-                                                        <td>5</td>
-                                                        <td>trocar</td>
-                                                        <td>cancelar</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>xxxxxxx</td>
-                                                        <td>R$ 49,90</td>
-                                                        <td>5</td>
-                                                        <td>trocar</td>
-                                                        <td>cancelar</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-
+                                    { purchases.map((purchase, index) => {
+                                        return (<>
+                                            <div className={`accordion ${purchase.selected ? '' : 'collapsed'}`}>
+                                                <input className="accordion-radio" id="accordion-radio" type="checkbox" onChange={(e) => {
+                                                    const newPurchases = purchases.slice();
+                                                    newPurchases[index].selected= e.target.checked;
+                                                    setPurchases(newPurchases);
+                                                }}/>
+                                                <label htmlFor="accordion-radio" className="accordion-header">
+                                                    <div>Pedido: {purchase.id}</div>
+                                                    <div>Status: {purchase.status}</div>
+                                                    <div>R$ {purchase.total_price}</div>
+                                                </label>
+                                                <div className="accordion-body">
+                                                    <div className="table-container">
+                                                        <table>
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Produto</th>
+                                                                <th>Valor</th>
+                                                                <th>Quantidade</th>
+                                                                <th></th>
+                                                                <th></th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                        { purchase.cart.items.map((item => {
+                                                            return (
+                                                                <tr>
+                                                                    <td>{item.product.name}</td>
+                                                                    <td>R$ {item.price}</td>
+                                                                    <td className="amount">{item.quantity}</td>
+                                                                    <td>trocar</td>
+                                                                    <td>cancelar</td>
+                                                                </tr>
+                                                            )
+                                                        }))}
+                                                        </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </>
+                                        )
+                                    })}
                             </WhiteBox>
                         </div>
                     </div>
