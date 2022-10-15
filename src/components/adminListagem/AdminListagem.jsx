414ab4d3ad7b802cf43d7aff92c695673edbf6db
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-15 11:51:59 -0300

    fix: admin listagem

--- src/components/adminListagem/AdminListagem.jsx
+++ src/components/adminListagem/AdminListagem.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import './style.scss';
 
 export function AdminListagem(props) {
-    const [search, setSearch] = useState('');
     return (
         <div className="admin-listagem">
             <h4>{props.title}</h4>
@@ -12,8 +11,8 @@ export function AdminListagem(props) {
                 {props.hasRegisterLink && <Link to={props.registerLink}>Cadastrar</Link>}
 
                 <fieldset>
-                    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-                    <button>Filtrar</button>
+                    <input type="text" value={props.search} onChange={(e) => props.setSearch(e.target.value)} />
+                    <button onClick={props.onClick}>Filtrar</button>
                 </fieldset>
 
                 <table>{props.children}</table>
