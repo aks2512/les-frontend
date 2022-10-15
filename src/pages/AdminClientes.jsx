a5d20f165b2d5f6411a6bf6e79e9b4acd7c0057d
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-15 13:08:13 -0300

    feat: list/inactive users

--- src/pages/AdminClientes.jsx
+++ src/pages/AdminClientes.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import api from "../api";
 import { AdminListagem } from "../components/adminListagem/AdminListagem";
 import { AdminSideMenu } from "../components/adminSideMenu/AdminSideMenu";
@@ -6,21 +7,34 @@ import { AdminSideMenu } from "../components/adminSideMenu/AdminSideMenu";
 export function AdminClientes() {
     const [loading, setLoading] = useState(true);
     const [clients, setClients] = useState();
+    const [search, setSearch] = useState("");
+
+    async function loadClients() {
+        const response = await api.get(`/users?search=${search}`, {
+            role: "usuario"
+        });
+
+        if(response.status === 201) {
+            setClients(response.data);
+            setLoading(false);
+        }
+    }
+
+    async function updateClient(e, client) {
+        e.preventDefault();
+        try{
+            const response = await api.put(`/users/${client.id}`, {
+                user: client
+            });
+            toast(response.data.message);
+            loadClients();
+        }catch(err){
+            toast.error(err.response.data.message);
+        }
+    }
 
     useEffect(() => {
-        async function loadClients() {
-            const response = await api.get('/users', {
-                role: "usuario"
-            });
-            console.log(response);
-            if(response.status === 201) {
-                setClients(response.data);
-                setLoading(false);
-            }
-        }
-
         loadClients();
-
     }, [])
     return (
         <main className="admin">
@@ -33,21 +47,36 @@ export function AdminClientes() {
                     <AdminListagem 
                         title="Clientes"
                         registerLink="/register-cliente"
+                        search={search}
+                        setSearch={setSearch}
+                        onClick={loadClients}
                     >
                         <thead>
                             <tr>
                                 <th>Nome</th>
                                 <th>Email</th>
                                 <th>Celular</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
                             {loading === false && clients.map((client) => 
                                 (
                                     <tr key={client.id}>
-                                        <td>{client.person.name}</td>
+                                        <td>{client.person?.name}</td>
                                         <td>{client.email}</td>
-                                        <td>{client.person.cellphone}</td>
+                                        <td>{client.person?.cellphone}</td>
+                                        <td>
+                                            <button onClick={(e)=>{
+                                                const active = !client.isActive;
+                                                updateClient(e, { 
+                                                    id: client.id,
+                                                    isActive: active 
+                                                });
+                                            }}>
+                                            {client.isActive ? 'Inativar' : 'Ativar'}
+                                            </button>
+                                        </td>
                                     </tr>
                                 )
                             )}
