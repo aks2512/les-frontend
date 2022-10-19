450606121a9490faaf8e8c9bf63aacc7a12b7d42
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-18 22:41:41 -0300

    fix: create new address cart wont submit properly

--- src/components/carrinhoEnderecos/CarrinhoEnderecos.jsx
+++ src/components/carrinhoEnderecos/CarrinhoEnderecos.jsx
@@ -15,40 +15,40 @@ import { CarrinhoEnderecoTipo } from "./partials/CarrinhoEnderecoTipo";
 
 import './style.scss';
 
-export function CarrinhoEnderecos({ 
+export function CarrinhoEnderecos({
     paymentAddress,
     deliveryAddress,
     setPaymentAddress,
     setDeliveryAddress,
- }) {
+}) {
     const { user } = useContext(Context);
-    const [showModalSelect, setShowModalSelect] = useState({open: false, type: 3});
-    const [showModalCreate, setShowModalCreate] = useState({open: false, type: 1, edit: false});
+    const [showModalSelect, setShowModalSelect] = useState({ open: false, type: 3 });
+    const [showModalCreate, setShowModalCreate] = useState({ open: false, type: 1, edit: false });
     const [createAddress, setCreateAddress] = useState(false);
 
     useEffect(() => {
         if (user) {
-            setPaymentAddress(user?.person?.addresses?.find(address => [1,3].includes(address.address_type.id)));
-            setDeliveryAddress(user?.person?.addresses?.find(address => [2,3].includes(address.address_type.id)));
+            setPaymentAddress(user?.person?.addresses?.find(address => [1, 3].includes(address.address_type.id)));
+            setDeliveryAddress(user?.person?.addresses?.find(address => [2, 3].includes(address.address_type.id)));
         }
-    }, [user]);
+    }, []);
 
     function selectAddress() {
-        if(showModalSelect.open){
+        if (showModalSelect.open) {
             return (
-                <CarrinhoEnderecoSelect 
-                    setDeliveryAddress={setDeliveryAddress} 
+                <CarrinhoEnderecoSelect
+                    setDeliveryAddress={setDeliveryAddress}
                     setPaymentAddress={setPaymentAddress}
                     type={showModalSelect.type}
                     onClose={(params) => setShowModalSelect({ open: false })}
                     onSelect={(address) => {
-                        if(showModalSelect.type == 1){
+                        if (showModalSelect.type == 1) {
                             setDeliveryAddress(address);
                         }
-                        
-                        if(showModalSelect.type == 2){
+
+                        if (showModalSelect.type == 2) {
                             setPaymentAddress(address);
-                        } 
+                        }
 
                         setShowModalSelect({ open: false });
                     }}
@@ -69,62 +69,62 @@ export function CarrinhoEnderecos({
                     </div>
                     <CarrinhoEnderecoTipo
                         type_name="Cobrança"
-                        register_func={(e) => { setShowModalCreate({open: true, type: 1}) }}
-                        select_func={(e) => { setShowModalSelect({open: true, type: 1}) }}     
+                        register_func={(e) => { setShowModalCreate({ open: true, type: 1 }) }}
+                        select_func={(e) => { setShowModalSelect({ open: true, type: 1 }) }}
                     />
-                    { paymentAddress ? <CarrinhoEndereco
+                    {paymentAddress ? <CarrinhoEndereco
                         name={paymentAddress?.name}
                         place={paymentAddress?.place}
                         city={paymentAddress?.city}
                         state={paymentAddress?.state}
                         cep={paymentAddress?.cep}
-                    /> : <p className="text-center">Nenhum endereço cadastrado</p> }
+                    /> : <p className="text-center">Nenhum endereço cadastrado</p>}
                     <hr></hr>
                     <CarrinhoEnderecoTipo
                         type_name="Entrega"
-                        register_func={(e) => { setShowModalCreate({open: true, type: 2}) }}
-                        select_func={(e) => { setShowModalSelect({open: true, type: 2}) }}     
+                        register_func={(e) => { setShowModalCreate({ open: true, type: 2 }) }}
+                        select_func={(e) => { setShowModalSelect({ open: true, type: 2 }) }}
                     />
-                    { deliveryAddress ? <CarrinhoEndereco
+                    {deliveryAddress ? <CarrinhoEndereco
                         type={deliveryAddress?.address_type}
                         name={deliveryAddress?.name}
                         place={deliveryAddress?.place}
                         city={deliveryAddress?.city}
                         state={deliveryAddress?.state}
                         cep={deliveryAddress?.cep}
-                    /> : <p className="text-center">Nenhum endereço cadastrado</p> }
+                    /> : <p className="text-center">Nenhum endereço cadastrado</p>}
                 </div>
             </div>
-            <Modal onClose={()=>{setShowModalCreate({...showModalCreate, open: false})}} isOpen={showModalCreate.open}>
-                <AddressForm onSubmit={async (e,{...params}) => {
+            <Modal onClose={() => { setShowModalCreate({ ...showModalCreate, open: false }) }} isOpen={showModalCreate.open}>
+                <AddressForm onSubmit={async (e, { ...params }) => {
                     e.preventDefault();
-                    if([1, 3].includes(Number(params.address_type_id))){
-                        setDeliveryAddress({...params, save: createAddress.save})
-                    } 
-                    
-                    if([2, 3].includes(Number(params.address_type_id))){
-                        setPaymentAddress({...params, save: createAddress.save})
+                    if ([1, 3].includes(Number(params.address_type_id))) {
+                        setDeliveryAddress({ ...params, save: createAddress.save })
                     }
 
-                    if(createAddress){
-                        try{
+                    if ([2, 3].includes(Number(params.address_type_id))) {
+                        setPaymentAddress({ ...params, save: createAddress.save })
+                    }
+
+                    if (createAddress) {
+                        try {
                             const response = await api.post(`addresses`, {
                                 ...params
-                            })
-                            toast.message(response.data.message);
-                        } catch(err) {
+                            });
+                            toast(response.data.message);
+                        } catch (err) {
                             toast.error(err.response.data.message || 'Falha ao cadastrar endereço')
                             return;
                         }
                     }
-                    
+
                     setCreateAddress(false)
                     setShowModalCreate(false)
                 }}>
                     <div className="cadastrar-endereco custom-form mb-4">
                         <Titulo title={'Deseja Salvar o Endereço na Sua Conta?'} />
-                        <button 
-                            className={createAddress ? 'button-true' : 'button-false'} 
+                        <button
+                            className={createAddress ? 'button-true' : 'button-false'}
                             onClick={
                                 (e) => {
                                     e.preventDefault()
@@ -136,7 +136,7 @@ export function CarrinhoEnderecos({
                         </button>
                     </div>
                 </AddressForm>
-            </Modal>  
+            </Modal>
         </div>
     );
 }
