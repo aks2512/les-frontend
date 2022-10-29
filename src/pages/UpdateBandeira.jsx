57612bf2f0f2cd788008159fb0a7f1bd330323be
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-29 11:33:46 -0300

    fix: update brands without updating image

--- src/pages/UpdateBandeira.jsx
+++ src/pages/UpdateBandeira.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import api from "../api";
 
 import { AdminCustomForm } from "../components/adminCustomForm/AdminCustomForm";
@@ -13,48 +14,53 @@ export function UpdateBandeira() {
     const [nomeDaBandeira, setNomeDaBandeira] = useState('');
     const [linkDaBandeira, setLinkDaBandeira] = useState('');
 
-    useEffect(() => {
-        async function loadData() {
-            if (id) {
-                const response = await api.get(`/brands?id=${id}`);
-                if(response.status === 201) {
-                    setNomeDaBandeira(response.data.name);
-                    setLinkDaBandeira(response.data.image);
-                }
-            } else {
-                navigate('/admin-bandeiras');
-            }
-        }
+    async function loadData() {
+        if (id) {
+            const response = await api.get(`/brands/${id}`);
 
-        loadData();
-    }, [id]);
+            console.log(response);
+            setNomeDaBandeira(response.data.name);
+            setLinkDaBandeira(response.data.image.split("/")[3]);
+        } else {
+            navigate('/admin-bandeiras');
+        }
+    }
 
     async function updateBrand(e) {
         e.preventDefault();
-        const fd = new FormData();
-        fd.append('image', linkDaBandeira, linkDaBandeira.name);
-        fd.append('name', nomeDaBandeira);
-        const response = await api.put('brands/'+ id, fd,
-            {
-                onUploadProgress: progressEvent => {
-                    console.log(Math.round(progressEvent.loaded / progressEvent.total * 100) + '%');
-                }
-            }
-        ).then(res => {
-            console.log(res);
-        })
 
-        if(response.status === 201) {
-            alert('Bandeira atualizada com sucesso!')
+        const fd = new FormData();
+
+        if (linkDaBandeira) {
+            fd.append('image', linkDaBandeira, linkDaBandeira.name);
+        }
+        fd.append('name', nomeDaBandeira);
+
+        try {
+            await api.put(`/brands/${id}`, fd,
+                {
+                    onUploadProgress: progressEvent => {
+                        console.log(Math.round(progressEvent.loaded / progressEvent.total * 100) + '%');
+                    }
+                }
+            );
+
+            toast('Atualizado com sucesso');
+        } catch (err) {
+            toast(err?.response?.data?.message || 'Erro ao atualizar a bandeira');
         }
 
     }
 
+    useEffect(() => {
+        loadData();
+    }, [id]);
+
     return (
         <main className="admin">
-            <div className="row w-100 px-0 m-0">    
+            <div className="row w-100 px-0 m-0">
                 <div className="col-12 col-xl-3 px-0">
-                    <AdminSideMenu/>
+                    <AdminSideMenu />
                 </div>
 
                 <div className="col-12 col-xl-9 px-0">
@@ -63,13 +69,13 @@ export function UpdateBandeira() {
 
                         <fieldset>
                             <label htmlFor="nome_da_bandeira">Nome da Bandeira</label>
-                            <input type="text" id="nome_da_bandeira" value={nomeDaBandeira} onChange={(e) => setNomeDaBandeira(e.target.value)}/>
+                            <input type="text" id="nome_da_bandeira" value={nomeDaBandeira} onChange={(e) => setNomeDaBandeira(e.target.value)} />
                         </fieldset>
 
                         <fieldset>
                             <label htmlFor="link_da_bandeira">Link da Bandeira</label>
                             <input type="file" id="link_da_bandeira"
-                            onChange={(e) => setLinkDaBandeira(e.target.files[0])}/>
+                                onChange={(e) => setLinkDaBandeira(e.target.files[0])} />
                         </fieldset>
 
                         <div className="btns">
