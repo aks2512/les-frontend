28683f519f4497ad5e52c89d70599ca4805e07b3
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-29 13:59:50 -0300

    fix: admin-´produtos

--- src/pages/AdminProdutos.jsx
+++ src/pages/AdminProdutos.jsx
@@ -18,7 +18,7 @@ export function AdminProdutos() {
 
     async function loadProducts() {
         const response = await api.get(`/products?search=${search}`);
-        if(response.status === 201) {
+        if (response.status === 201) {
             setProducts(response.data);
             setLoading(false);
         }
@@ -26,24 +26,24 @@ export function AdminProdutos() {
 
     async function deleteProduct(e, id) {
         e.preventDefault();
-        try{
+        try {
             const response = await api.delete('/products/' + id);
             toast('Deletado com sucesso')
             loadProducts();
-        } catch(error){
+        } catch (error) {
             toast.error(error?.message)
         }
     }
 
     return (
         <main className="admin">
-            <div className="row w-100 px-0 m-0">    
+            <div className="row w-100 px-0 m-0">
                 <div className="col-12 col-xl-3 px-0">
-                    <AdminSideMenu/>
+                    <AdminSideMenu />
                 </div>
 
                 <div className="col-12 col-xl-9 px-0">
-                    <AdminListagem 
+                    <AdminListagem
                         title="Produtos"
                         hasRegisterLink={true}
                         registerLink="/register-produto"
@@ -62,16 +62,16 @@ export function AdminProdutos() {
                         </thead>
                         <tbody>
 
-                            {loading === false && products.map((product) => 
-                                (
-                                    <tr key={product.id}>
-                                        <td className="small">{product.id}</td>
-                                        <td><img width="100" height="80" src={'http://localhost:3333/files/' + product.image} alt="" /></td>
-                                        <td>{product.name}</td>
-                                        <td className="btn"><Link to={'/update-produto?id='+product.id}>Editar</Link></td>
-                                        <td className="btn"><button onClick={(e) => deleteProduct(e, product.id)}>Excluir</button></td>
-                                    </tr>
-                                )
+                            {loading === false && products.map((product) =>
+                            (
+                                <tr key={product.id}>
+                                    <td className="small">{product.id}</td>
+                                    <td><img width="100" height="80" src={product.image_url} alt="" /></td>
+                                    <td>{product.name}</td>
+                                    <td className="btn"><Link to={'/update-produto?id=' + product.id}>Editar</Link></td>
+                                    <td className="btn"><button onClick={(e) => deleteProduct(e, product.id)}>Excluir</button></td>
+                                </tr>
+                            )
                             )}
                         </tbody>
                     </AdminListagem>
