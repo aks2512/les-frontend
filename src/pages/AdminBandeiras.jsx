c373bfa4ad0d3a084378189cafdd387376326051
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-29 11:33:53 -0300

    fix: list brands

--- src/pages/AdminBandeiras.jsx
+++ src/pages/AdminBandeiras.jsx
@@ -14,10 +14,11 @@ export function AdminBandeiras() {
 
     async function loadBrands() {
         const response = await api.get(`/brands?search=${search}`);
-        if(response.status === 201) {
+        if (response.status === 201) {
             setBrands(response.data);
             setLoading(false);
         }
+        console.log(response.data);
     }
 
     useEffect(() => {
@@ -26,24 +27,24 @@ export function AdminBandeiras() {
 
     async function deleteBrand(e, id) {
         e.preventDefault();
-        try{
+        try {
             const response = await api.delete('/brands/' + id);
             toast('Deletado com sucesso')
             loadBrands();
-        } catch(error){
+        } catch (error) {
             toast.error(error.response.data.message)
         }
     }
 
     return (
         <main className="admin">
-            <div className="row w-100 px-0 m-0">    
+            <div className="row w-100 px-0 m-0">
                 <div className="col-12 col-xl-3 px-0">
-                    <AdminSideMenu/>
+                    <AdminSideMenu />
                 </div>
 
                 <div className="col-12 col-xl-9 px-0">
-                    <AdminListagem 
+                    <AdminListagem
                         title="Bandeiras"
                         hasRegisterLink={true}
                         registerLink="/register-bandeira"
@@ -61,15 +62,15 @@ export function AdminBandeiras() {
                         </thead>
                         <tbody>
 
-                            {loading === false && brands.map((brand) => 
-                                (
-                                    <tr key={brand.id}>
-                                        <td><img width="100" src={brand.image} alt="" /></td>
-                                        <td>{brand.name}</td>
-                                        <td className="btn"><Link to={'/update-bandeira?id='+brand.id}>Editar</Link></td>
-                                        <td className="btn"><button onClick={(e) => deleteBrand(e, brand.id)}>Excluir</button></td>
-                                    </tr>
-                                )
+                            {loading === false && brands.map((brand) =>
+                            (
+                                <tr key={brand.id}>
+                                    <td><img width="100" src={brand.image_url} alt="" /></td>
+                                    <td>{brand.name}</td>
+                                    <td className="btn"><Link to={'/update-bandeira?id=' + brand.id}>Editar</Link></td>
+                                    <td className="btn"><button onClick={(e) => deleteBrand(e, brand.id)}>Excluir</button></td>
+                                </tr>
+                            )
                             )}
                         </tbody>
                     </AdminListagem>
